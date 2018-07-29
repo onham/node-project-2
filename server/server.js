b8c1e2b24dce6da446300f3158b193ef0d6397b9
Quan Nham <quannham6@gmail.com>
2018-07-29 01:25:51 -0400

    GET request update

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');  //takes our strings and converts it to a js object
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -7,7 +8,9 @@ const { User } = require('./models/user');
 
 const app = express();
 
+
 app.use(bodyParser.json())     //configuring middleware
+
 
 app.post('/todos', (req, res) => {   //making POST request to url of /todos, user input will be our text prop
 	const todo = new Todo({
@@ -21,6 +24,7 @@ app.post('/todos', (req, res) => {   //making POST request to url of /todos, use
  	});
 })
 
+
 app.get('/todos', (req, res) => {  //making GET request to url
 	Todo.find().then((todos) => {   //model method -- finding and retrieving all entries from our 'todos' collection
 		res.send({     //sending back an object with our array in that object -- instead of sending back just an array, object provides more flexibility for the future
@@ -31,9 +35,29 @@ app.get('/todos', (req, res) => {  //making GET request to url
 	})
 })
 
+
+app.get('/todos/:id', (req, res) => {
+	// res.send(req.params);    //so here we are sending back the key-value pair associated with param designated after :
+	const id = req.params.id
+
+	if (!ObjectID.isValid(id)){  //this method returns true if valid and false otherwise
+		return res.status(404).send();
+	}    
+
+	Todo.findById(id).then((todo) => {
+		if (!todo) {
+			return res.status(404).send();
+		}
+		res.send(todo);
+
+	}).catch((e) => res.status(400).send());
+});
+
+
 app.listen(3000, () => {
 	console.log('starting on port 3000')
 });
+
 
 module.exports = {
 	app
